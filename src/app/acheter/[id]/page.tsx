bf7e52c606c3bef60d5ea2a80a269dad55082c41
SORY IBRAHIMA SISSOKO <soryisissoko6@gmail.com>
2025-06-18 22:56:15 +0000

    Forcer le changement pour relancer le build Vercel

--- src/app/acheter/[id]/page.tsx
+++ src/app/acheter/[id]/page.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Page({ params }: Props) {
   return (
     <div>
-      <h1>Produit ID : {params.id}</h1>
+      <h1>ID du produit : {params.id}</h1>
     </div>
   );
 }
