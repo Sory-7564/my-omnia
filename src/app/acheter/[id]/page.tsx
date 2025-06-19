15ad36f682c0caa1df9a6209a1bd89c24c9b47cb
SORY IBRAHIMA SISSOKO <soryisissoko6@gmail.com>
2025-06-19 18:33:21 +0000

    Fix params typing + generateStaticParams for Vercel

--- src/app/acheter/[id]/page.tsx
+++ src/app/acheter/[id]/page.tsx
@@ -1,9 +1,10 @@
 import ProduitDetail from './ProduitDetail'
 
-export default function Page({
-  params,
-}: {
-  params: { id: string }
-}) {
+export default function Page({ params }: { params: { id: string } }) {
   return <ProduitDetail id={params.id} />
+}
+
+// Ceci est essentiel pour les pages dynamiques
+export async function generateStaticParams() {
+  return []
 }
