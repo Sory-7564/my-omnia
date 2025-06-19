743ce60883c4e9a675a710ccf71e2187aeadf150
SORY IBRAHIMA SISSOKO <soryisissoko6@gmail.com>
2025-06-19 18:38:47 +0000

    Fix bug Vercel params type + suppression metadata

--- src/app/acheter/[id]/head.tsx
+++
@@ -1,7 +0,0 @@
-import { Metadata } from 'next'
-
-export default function Head({ params }: { params: { id: string } }): Metadata {
-  return {
-    title: `Produit ${params.id}`,
-  }
-}
--- src/app/acheter/[id]/page.tsx
+++ src/app/acheter/[id]/page.tsx
@@ -4,7 +4,3 @@ export default function Page({ params }: { params: { id: string } }) {
   return <ProduitDetail id={params.id} />
 }
 
-// Ceci est essentiel pour les pages dynamiques
-export async function generateStaticParams() {
-  return []
-}
