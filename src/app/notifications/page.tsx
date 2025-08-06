1e35b96164a49e37f44d11e054b84337cb8d7cbe
SORY IBRAHIMA SISSOKO <soryisissoko6@gmail.com>
2025-08-06 12:59:16 +0000

    Supprimer temporairement la page notifications pour réussir le déploiement

--- src/app/notifications/page.tsx
+++
@@ -1,9 +0,0 @@
-// 'use client'
-
-export default function NotificationsPage() {
-  return (
-    <div style={{ color: 'white', padding: 20 }}>
-      <h1>Notifications désactivées temporairement</h1>
-    </div>
-  )
-}
