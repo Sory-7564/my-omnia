2e990d16292ad7c0eaf4070c7f24602bf6bd8264
Sory-7564 <soryisissoko6@gmail.com>
2026-02-12 01:19:36 +0000

    Update page.tsx

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -2,32 +2,8 @@
 
 export const dynamic = 'force-dynamic'
 
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { supabase } from '@/lib/supabaseClient'
+import ClientCallback from './ClientCallback'
 
 export default function AuthCallbackPage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleCallback = async () => {
-      const params = new URLSearchParams(window.location.search)
-      const code = params.get('code')
-
-      if (code) {
-        await supabase.auth.exchangeCodeForSession(code)
-        router.replace('/')
-      } else {
-        router.replace('/auth/login')
-      }
-    }
-
-    handleCallback()
-  }, [router])
-
-  return (
-    <div className="min-h-screen flex items-center justify-center text-white bg-black">
-      Confirmation en cours...
-    </div>
-  )
+  return <ClientCallback />
 }
